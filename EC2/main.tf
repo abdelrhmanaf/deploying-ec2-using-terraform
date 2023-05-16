df0d0aff946f5c2db30ec15a313a7cfa03f0356a
Abdelrhmanaf <abdelrhmanaiman99@gmail.com>
2023-05-16 11:50:41 +0300

    EC2 with port 22 and 8080

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,8 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+  
+}
 
 resource "aws_instance" "TerraformInstance" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "key"
+  key_name      = "AAF"
+  vpc_security_group_ids = ["${data.aws_security_group.Web-server.id}"]
   tags = {
     Name = "Terraform Ec2"
   }
@@ -11,4 +16,40 @@ resource "aws_instance" "TerraformInstance" {
     #!/bin/bash
     sudo apt update -y
     EOF
+}
+resource "aws_security_group" "Web-server" {
+  name = "Web server"
+  description = "Allow traffic on port 22 and port 8080"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "Allow for SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Port 8080 used for web servers"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "virtual port that computers use to divert network traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
--- EC2/variables.tf
+++ EC2/variables.tf
@@ -6,10 +6,16 @@ variable "aws_region" {
 variable "ami"{
     description = "ami for ubuntu"
     type = string
-    default = "ami"
+    default = "ami-007855ac798b5175e"
 }
 variable "instance_type"{
     description = "type of the Instance"
     type = string
     default = "t2.micro"
+}
+variable "vpc_id" {
+
+description = "VPC"
+type = string
+default = "vpc"  
 }
